fd9006e28a7498d6daee77cd5bc3ab7764a82ab4
Brian Zambrano <brianz@gmail.com>
2015-01-21 22:54:27 -0800

    Updates

--- cpdots.sh
+++ cpdots.sh
@@ -1,9 +1,9 @@
 for fn in bash_key_bindings bashrc gitconfig gitignore screenrc vimrc; do
     #cp ~/.$fn $fn;
+    cp ~/.$fn ~/.$fn.bak;
     cp $fn ~/.$fn;
 done
 
 # Setup new machine
-curl https://raw.github.com/git/git/master/contrib/completion/git-completion.sh -o ~/.git-completion
-curl https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt
-
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o .git-completion
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o .git-prompt
